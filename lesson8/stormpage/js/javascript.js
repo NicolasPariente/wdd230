c11207b62f93bfdb8d33e41b89011dd3b1e64f58
NicolasPariente <par21022@byui.edu>
2021-11-09 09:55:01 -0300

    correcting errors

--- lesson8/stormpage/js/javascript.js
+++ lesson8/stormpage/js/javascript.js
@@ -57,8 +57,9 @@ function adjustRating(rating) {
 }
 
 function check_Alpha(letters) {
-  let regex = /[a-zA-Z]{5,30}/;
-  if (regex.test(letters.name.value) == false) {
+  //let regex = /[a-zA-Z]{5,30}/;
+  let regex = /\d+/;
+  if (regex.test(letters.name.value) == true) {
     alert("Your full name should have more than 5 characters and only letters");
     letters.name.focus();
     return false;
