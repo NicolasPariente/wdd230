8a02ac976edb86b7476eaeeea3305ead64bb449e
NicolasPariente <par21022@byui.edu>
2021-11-11 11:43:22 -0300

    update prophets page

---
+++ lesson9/prophets/js/prophets.js
@@ -0,0 +1,32 @@
+const requestURL =
+  "https://byui-cit230.github.io/lessons/lesson-09/data/latter-day-prophets.json";
+
+fetch(requestURL)
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (jsonObject) {
+    console.table(jsonObject); // temporary checking for valid response and data parsing
+    const prophets = jsonObject["prophets"];
+    for (let i = 0; i < prophets.length; i++) {
+      let card = document.createElement("section");
+
+      let h2 = document.createElement("h2");
+      h2.textContent = prophets[i].name + " " + prophets[i].lastname;
+      card.appendChild(h2);
+
+      let h3 = document.createElement("h3");
+      h3.textContent = "Date of Birth:" + " " + prophets[i].birthdate;
+      card.appendChild(h3);
+
+      h3 = document.createElement("h3");
+      h3.textContent = "Place of Birth:" + " " + prophets[i].birthplace;
+      card.appendChild(h3);
+
+      let img = document.createElement("img");
+      img.setAttribute("src", prophets[i].imageurl);
+      card.appendChild(img);
+
+      document.querySelector("div.cards").appendChild(card);
+    }
+  });
