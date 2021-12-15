5d267966b94d8820fa279614ced51da8ed2c02cf
NicolasPariente <par21022@byui.edu>
2021-12-14 21:25:20 -0300

    finish_homepage

--- finalproject/scripts/script.js
+++ finalproject/scripts/script.js
@@ -3,3 +3,8 @@ function toggleMenu() {
     .getElementsByClassName("navigation")[0]
     .classList.toggle("responsive");
 }
+let d = new Date();
+let fullyear = d.getFullYear();
+document.querySelector(".year").textContent = fullyear;
+let modified = document.lastModified;
+document.querySelector("#modify").textContent = modified;
--- finalproject/scripts/windchill.js
+++ finalproject/scripts/windchill.js
@@ -1,114 +1,60 @@
-const apiURL =
-  "https://api.openweathermap.org/data/2.5/weather?id=5604473&appid=ae2888a2c00262c804233e7ec092c22a";
-fetch(apiURL)
+const apiURL2 =
+  "https://api.openweathermap.org/data/2.5/onecall?lat=-32.36667&lon=-54.18333&exclude=hourly,minutely&appid=ae2888a2c00262c804233e7ec092c22a";
+fetch(apiURL2)
   .then((response) => response.json())
   .then((jsObject) => {
-    console.log(jsObject);
-    document.querySelector(".condition").innerHTML = jsObject.weather[0].main;
+    information = jsObject;
+    console.log(information);
+    let current = information.current;
+    let temp = parseInt((9 / 5) * (current.temp - 273) + 32);
 
-    farenheit = (9 / 5) * (parseFloat(jsObject.main.temp_max) - 273) + 32;
-    document.querySelector(".high").innerHTML = farenheit.toFixed(2);
+    let description = current.weather[0].main;
 
-    document.querySelector(".humidity").innerHTML = jsObject.main.humidity;
+    let humidity = current.humidity;
 
-    document.querySelector(".speed").innerHTML = jsObject.wind.speed;
-
-    let high = document.getElementsByClassName("high")[0].innerHTML;
-    console.log(high);
-    let speed = parseFloat(
-      document.getElementsByClassName("speed")[0].innerHTML
-    );
-    console.log(speed);
-    let chill = document.getElementsByClassName("chill")[0];
-    let result = parseFloat(
-      35.74 +
-        0.6215 * high -
-        35.75 * Math.pow(speed, 0.16) +
-        0.4275 * high * Math.pow(speed, 0.16)
-    );
-    chill.innerHTML = result.toFixed(2);
-    console.log(result);
-  });
-
-const apiURLforecast =
-  "https://api.openweathermap.org/data/2.5/forecast?id=5604473&appid=ae2888a2c00262c804233e7ec092c22a";
-fetch(apiURLforecast)
-  .then((response) => response.json())
-  .then((jsObject) => {
-    list_days = jsObject.list;
-    console.log(list_days);
-    let list_correct_time = [];
-    let moment = 0;
-    for (let i = 0; i < list_days.length; i++) {
-      moment = list_days[i].dt_txt;
-      time = moment.split(" ");
-      if (time[1] == "18:00:00") {
-        list_correct_time.push(list_days[i]);
-      }
+    let srcimg =
+      "https://openweathermap.org/img/w/" + current.weather[0].icon + ".png";
+    if (current.weather[0].icon == "01d" || current.weather[0].icon == "01n") {
+      srcimg = "images/sun.png";
     }
-    console.log(list_correct_time);
-    let list_of_temperatures = [];
-    let list_of_images_src = [];
-    let list_of_images_desc = [];
-    let temp = 0;
-    let image = "";
-    let desc = "";
-    for (let i = 0; i < list_correct_time.length; i++) {
-      temp = list_correct_time[i].main.temp;
-      temp = parseInt((9 / 5) * (temp - 273) + 32);
-      list_of_temperatures.push(temp);
-      image =
+    if (current.weather[0].icon == "02d" || current.weather[0].icon == "02n") {
+      srcimg = "images/cloudsun.png";
+    }
+    if (current.weather[0].icon == "03d" || current.weather[0].icon == "03n") {
+      srcimg = "images/clouds.png";
+    }
+    if (current.weather[0].icon == "04d" || current.weather[0].icon == "04n") {
+      srcimg = "images/brokenclouds.png";
+    }
+    if (
+      current.weather[0].icon == "09d" ||
+      current.weather[0].icon == "09n" ||
+      current.weather[0].icon == "10d" ||
+      current.weather[0].icon == "10n"
+    ) {
+      srcimg = "images/rain.png";
+    }
+    if (current.weather[0].icon == "11d" || current.weather[0].icon == "11n") {
+      srcimg = "images/storm.png";
+    }
+
+    document.querySelector("#forecast_img").src = srcimg;
+    document.querySelector("#temp").innerHTML = temp;
+    document.querySelector("#description").innerHTML = description;
+    document.querySelector("#humidity").innerHTML = humidity;
+    for (let i = 1; i < 4; i++) {
+      srcimg =
         "https://openweathermap.org/img/w/" +
-        list_correct_time[i].weather[0].icon +
+        information.daily[i].weather[0].icon +
         ".png";
-      list_of_images_src.push(image);
-      desc = list_correct_time[i].weather[0].description;
-      list_of_images_desc.push(desc);
+      temp = information.daily[i].temp.day;
+      temp = parseInt((9 / 5) * (temp - 273) + 32);
+      let idimg = `#forecast_img${i}`;
+      let idtemp = `#forecast${i}`;
+      document.querySelector(idimg).src = srcimg;
+      document.querySelector(idtemp).innerHTML = temp;
     }
-    console.log(list_of_images_desc);
-    document.querySelector(
-      "#forecast_one"
-    ).innerHTML = `${list_of_temperatures[0]}&deg;F`;
-    document.querySelector("#forecast_img_one").src = list_of_images_src[0];
-    document
-      .getElementById("forecast_img_one")
-      .setAttribute("alt", list_of_images_desc[0]);
-
-    document.querySelector(
-      "#forecast_two"
-    ).innerHTML = `${list_of_temperatures[1]}&deg;F`;
-    document
-      .querySelector("#forecast_img_two")
-      .setAttribute("src", list_of_images_src[1]);
-    document
-      .getElementById("forecast_img_two")
-      .setAttribute("alt", list_of_images_desc[1]);
-
-    document.querySelector(
-      "#forecast_three"
-    ).innerHTML = `${list_of_temperatures[2]}&deg;F`;
-    document.querySelector("#forecast_img_three").src = list_of_images_src[2];
-    document
-      .getElementById("forecast_img_three")
-      .setAttribute("alt", list_of_images_desc[2]);
-
-    document.querySelector(
-      "#forecast_four"
-    ).innerHTML = `${list_of_temperatures[3]}&deg;F`;
-    document.querySelector("#forecast_img_four").src = list_of_images_src[3];
-    document
-      .getElementById("forecast_img_four")
-      .setAttribute("alt", list_of_images_desc[3]);
-
-    document.querySelector(
-      "#forecast_five"
-    ).innerHTML = `${list_of_temperatures[4]}&deg;F`;
-    document.querySelector("#forecast_img_five").src = list_of_images_src[4];
-    document
-      .getElementById("forecast_img_five")
-      .setAttribute("alt", list_of_images_desc[4]);
   });
-
 let date = new Date();
 let day = date.getDay();
 console.log(day);
@@ -123,12 +69,8 @@ let correct_index = function (day) {
 day_index_correct = day_indexes.map(correct_index);
 console.log(day_index_correct);
 
-document.querySelector("#day1").innerHTML = days[day_index_correct[0]];
+document.querySelector("#day1").innerHTML = days[day_index_correct[1]];
 
-document.querySelector("#day2").innerHTML = days[day_index_correct[1]];
+document.querySelector("#day2").innerHTML = days[day_index_correct[2]];
 
-document.querySelector("#day3").innerHTML = days[day_index_correct[2]];
-
-document.querySelector("#day4").innerHTML = days[day_index_correct[3]];
-
-document.querySelector("#day5").innerHTML = days[day_index_correct[4]];
+document.querySelector("#day3").innerHTML = days[day_index_correct[3]];
